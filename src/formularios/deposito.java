b5b4e0bac0c4df0c2a0bcc2fd86cbba5adaf64c2
Guillermo Home <guille.work.94@gmail.com>
2025-08-05 15:11:21 -0400

    Primer commit

---
+++ src/formularios/deposito.java
@@ -0,0 +1,333 @@
+package formularios;
+
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.YES_OPTION;
+import prg.conectDB;
+
+public class deposito extends javax.swing.JDialog {
+    
+    conectDB con; // Traer la clase de conexion
+    ResultSet rs; // Resultados de SQL definidos en conecDB
+    String operacion = ""; 
+
+    public deposito(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        con = new conectDB(); // Instancia de la clase de conexion
+        con.conectar(); // Metodo de conexion de la clase conecDB
+        desa_inicio(); // Metodo de inicio de la pantalla
+        setLocationRelativeTo(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelNice1 = new org.edisoncor.gui.panel.PanelNice();
+        panelCurves1 = new org.edisoncor.gui.panel.PanelCurves();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        btnagregar = new javax.swing.JButton();
+        btnmodificar = new javax.swing.JButton();
+        btneliminar = new javax.swing.JButton();
+        btnimprimir = new javax.swing.JButton();
+        btnguardar = new javax.swing.JButton();
+        btncancelar = new javax.swing.JButton();
+        buttonNice1 = new org.edisoncor.gui.button.ButtonNice();
+        labelMetric1 = new org.edisoncor.gui.label.LabelMetric();
+        labelMetric2 = new org.edisoncor.gui.label.LabelMetric();
+        txtdescripcion = new org.edisoncor.gui.textField.TextFieldRectBackground();
+        txtcodigo = new org.edisoncor.gui.textField.TextFieldRectBackground();
+        txtbuscar = new org.edisoncor.gui.textField.TextFieldRectBackground();
+        labelMetric3 = new org.edisoncor.gui.label.LabelMetric();
+        btnbuscar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        panelNice1.setBackground(new java.awt.Color(153, 153, 153));
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Formulario Depósito"));
+        jPanel1.setOpaque(false);
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Acciones"));
+        jPanel2.setOpaque(false);
+
+        btnagregar.setText("Agregar");
+
+        btnmodificar.setText("Modificar");
+        btnmodificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnmodificarActionPerformed(evt);
+            }
+        });
+
+        btneliminar.setText("Eliminar");
+
+        btnimprimir.setText("Imprimir");
+
+        btnguardar.setText("Guardar");
+
+        btncancelar.setText("Cancelar");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnmodificar, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btneliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnimprimir, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnguardar, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btncancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnagregar, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(54, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(btnagregar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnmodificar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btneliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(44, 44, 44)
+                .addComponent(btnimprimir, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+                .addComponent(btnguardar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btncancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(48, 48, 48))
+        );
+
+        buttonNice1.setBackground(new java.awt.Color(0, 102, 255));
+        buttonNice1.setText("Salir");
+        buttonNice1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonNice1ActionPerformed(evt);
+            }
+        });
+
+        labelMetric1.setText("Código: ");
+        labelMetric1.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+
+        labelMetric2.setText("Descripción: ");
+        labelMetric2.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+
+        txtdescripcion.setDescripcion("Ingrese la descripción");
+        txtdescripcion.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+
+        txtcodigo.setDescripcion("Cód.");
+        txtcodigo.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+
+        txtbuscar.setDescripcion("Ingrese la descripción");
+        txtbuscar.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+
+        labelMetric3.setText("Buscar");
+        labelMetric3.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+
+        btnbuscar.setText("Buscar");
+
+        jTable1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null},
+                {null, null}
+            },
+            new String [] {
+                "Código", "Descripción"
+            }
+        ));
+        jTable1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jScrollPane1.setViewportView(jTable1);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(buttonNice1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(labelMetric2, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(labelMetric1, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(labelMetric3, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(34, 34, 34)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtcodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                            .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                            .addComponent(btnbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(txtdescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, 338, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 553, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(labelMetric1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtcodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(labelMetric2, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtdescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(labelMetric3, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(42, 42, 42)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(buttonNice1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(53, Short.MAX_VALUE))
+        );
+
+        panelCurves1.add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        panelNice1.add(panelCurves1, java.awt.BorderLayout.CENTER);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelNice1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelNice1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void desa_inicio(){
+        
+        btnguardar.setEnabled(false);
+        txtcodigo.setEnabled(false);
+        txtdescripcion.setEnabled(false);
+        
+    }
+    
+    private void desa_botones(int a){
+        
+        switch(a){
+            case 1:
+                btnagregar.setEnabled(false);
+                btnmodificar.setEnabled(false);
+                btneliminar.setEnabled(false);
+                btnimprimir.setEnabled(false);
+                buttonNice1.setEnabled(false);
+            break;
+            case 2:
+                btnagregar.setEnabled(true);
+                btnmodificar.setEnabled(true);
+                btneliminar.setEnabled(true);
+                btnimprimir.setEnabled(true);
+                buttonNice1.setEnabled(true);
+            break;
+        }
+        
+    }
+    
+    private void btnmodificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnmodificarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnmodificarActionPerformed
+
+    private void buttonNice1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonNice1ActionPerformed
+        
+        int mensaje = JOptionPane.showConfirmDialog(this,"Desea salir?","Atención",JOptionPane.YES_NO_OPTION);
+        if (mensaje == JOptionPane.YES_OPTION){
+            //System.exit(WIDTH); // Para cerrar totalmente el sistema.
+            dispose(); // Para cerrar ventanas de opciones o formularios sin parar el sistema.
+        }
+        
+    }//GEN-LAST:event_buttonNice1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(deposito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(deposito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(deposito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(deposito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the dialog */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                deposito dialog = new deposito(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnagregar;
+    private javax.swing.JButton btnbuscar;
+    private javax.swing.JButton btncancelar;
+    private javax.swing.JButton btneliminar;
+    private javax.swing.JButton btnguardar;
+    private javax.swing.JButton btnimprimir;
+    private javax.swing.JButton btnmodificar;
+    private org.edisoncor.gui.button.ButtonNice buttonNice1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private org.edisoncor.gui.label.LabelMetric labelMetric1;
+    private org.edisoncor.gui.label.LabelMetric labelMetric2;
+    private org.edisoncor.gui.label.LabelMetric labelMetric3;
+    private org.edisoncor.gui.panel.PanelCurves panelCurves1;
+    private org.edisoncor.gui.panel.PanelNice panelNice1;
+    private org.edisoncor.gui.textField.TextFieldRectBackground txtbuscar;
+    private org.edisoncor.gui.textField.TextFieldRectBackground txtcodigo;
+    private org.edisoncor.gui.textField.TextFieldRectBackground txtdescripcion;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/prg/conectDB.java
@@ -0,0 +1,133 @@
+/* Conectar a la Base de Datos */
+package prg;
+
+//import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class conectDB {
+    
+    Connection con; // Variable administrador de base de datos
+    Statement state; // Variable para ejecutar sql
+    ResultSet resultado; // Variable que recibe los resultados de las sentencias sq
+
+    // Variables para conexión
+    String host = "jdbc:postgres://localhost/";
+    String bd = "proyecto";
+    String user = "postgres";
+    String pass = "123456";
+    
+    public Connection conectar(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            try {
+                con = DriverManager.getConnection(host+bd,user,pass);
+            } catch (SQLException ex) {
+                Logger.getLogger(conectDB.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(conectDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+   // Listar Datos
+    public ResultSet Listar(String consulta) {
+        try {
+            state = con.createStatement(ResultSet.CONCUR_READ_ONLY,ResultSet.TYPE_SCROLL_INSENSITIVE);
+            resultado = state.executeQuery(consulta);
+        } catch (SQLException ex) {
+            Logger.getLogger(conectDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
+    
+    // Buscar Datos
+    public boolean busqueda(String query) {
+        try {
+            state = con.createStatement();
+            resultado = state.executeQuery(query);
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(conectDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    // Insertar Datos
+    public void insertar_datos(String tabla, String campos, String valores, int mensaje){
+        try {
+            state = con.createStatement();
+            state.executeUpdate("insert into " +tabla+ "("+campos+ ")"
+                    +"values("+valores+")");
+            switch(mensaje){
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Se ha grabado exitosamente", "Atención",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                case 2:
+                    break;
+            }
+        } catch (SQLException ex) {
+            System.out.println("insert into " +tabla+ "("+campos+ ")"
+                    +"values("+valores+")");
+            JOptionPane.showMessageDialog(null, "Error en la operación", "Atención",
+                            JOptionPane.WARNING_MESSAGE);
+            javax.swing.JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+    
+    // Modificar Datos
+    public void actualizar_datos(String tabla, String camposAct, String codigo, int mensaje) {
+        try {
+            state = con.createStatement();
+            state.executeUpdate("update " + tabla + " set " + camposAct + " where "
+                    + codigo, mensaje);
+            switch (mensaje) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Se ha actualizado exitosamente", "Atención",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Se ha anulado correctamente", "Atención",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    break;
+            }
+        } catch (SQLException ex) {
+            System.out.println("update " + tabla + " set " + camposAct + " where "
+                    + codigo);
+            javax.swing.JOptionPane.showMessageDialog(null, ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error en la operación", "Atención",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    
+    // Eliminar Datos
+    public void borrar_datos(String tabla, String campoCodigo, String codigo) {
+        try {
+            state = con.createStatement();
+            state.executeUpdate("delete from " + tabla + " where " + campoCodigo
+                    + " = " + codigo + ";");
+            JOptionPane.showMessageDialog(null, "Se ha eliminado correctamente", "Atención",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException ex) {
+            System.out.println("delete from " + tabla + " where " + campoCodigo
+                    + " = " + codigo + ";");
+            javax.swing.JOptionPane.showMessageDialog(null, ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error en la operación", "Atención",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+}
